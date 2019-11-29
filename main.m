d4e253de98cdd317dfb392973ccd10cddf029a1d
MonGreL94 <a.scaldaferri2@studenti.unisa.it>
2019-11-29 18:31:55 +0100

    Plotting fv

--- main.m
+++ main.m
@@ -6,7 +6,7 @@ im = imread('checkerboard.jpg');
 % figure; imshow(imnorm(sg));
 % figure; imshow(sgc);
 
-metric = 'euclidean';
+metric = 'chessboard';
 
 rows = size(sg, 1) + 2;
 cols = size(sg, 2) + 2;
@@ -37,42 +37,33 @@ M = get_max(abs(dif), 1);
 [r, c] = find(M);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% DEBUGGING %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% auxdbg = zeros(rows, cols, 'double');
+% auxdbg(cy, cx) = 1;
+% auxdbg = bwdist(auxdbg, metric);
+% % figure; imshow(imnorm(auxdbg));
 % for i=2:400:rows - 1
 %     for j=2:400:cols - 1
 %         d = ceil(abs(dif(i, j)))
-%         subdif = dif(i - d:i + d,j - d:j + d)
-%         subaux = aux(cy - d:cy + d, cx - d:cx + d)
-%         subaux = (subaux > (d - 1)) & (subaux <= d)
+%         subdif = dif(i-d:i+d, j-d:j+d)
+%         subauxdbg = auxdbg(cy-d:cy+d, cx-d:cx+d)
+%         subauxdbg = (subauxdbg > (d - 1)) & (subauxdbg <= d)
 %         if strcmp(metric, 'chessboard')
 %             fv = [subdif(1, :).'; subdif(2:end, end); flip(subdif(end, 1:end-1)).'; flip(subdif(2:end-1, 1))]
 %         elseif strcmp(metric, 'cityblock')
 %             fv = [diag(subdif(1:d+1, d+1:end)); diag(rot90(subdif(d+2:end, d+1:end-1))); flip(diag(subdif(d+1:end-1, 1:d))); diag(rot90(subdif(2:d, 2:d), -1))]
 %         else
-%             subaux = rot90(aux(cy:cy + d, cx:cx + d));
-%             subaux = (subaux > (d - 1)) & (subaux <= d);
-%             [ra, ca] = find(subaux == 1);
-%             
-%             d1 = subdif(1:d+1, d+1:end);
-%             d2 = rot90(subdif(d+1:end, d+1:end));
-%             d3 = rot90(rot90(subdif(d+1:end, 1:d+1)));
-%             d4 = rot90(subdif(1:d+1, 1:d+1), -1);
-%             
-%             f1 = diag(d1(ra, ca));
-%             f2 = diag(d2(ra, ca));
-%             f3 = diag(d3(ra, ca));
-%             f4 = diag(d4(ra, ca));
-%             
-%             fv = [f1; f2(2:end); f3(2:end); f4(2:end-1)]
+%             subaux = aux(end-d:end, 1:1+d);
+%             [ra, ca] = find((subaux > (d - 1)) & (subaux <= d));
+%             fv = [diag(subdif(ra, ca + d)); diag(subdif(d + d + 2 - flip(ra(1:end-1)), flip(ca(1:end-1)) + d)); diag(subdif(d + d + 2 - ra(2:end), d + 2 - ca(2:end))); diag(subdif(flip(ra(2:end-1)), d + 2 - flip(ca(2:end-1))))]
 %         end
 %     end
 % end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% DEBUGGING %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+X = zeros((rows * 2) + ((cols - 2) * 2) + 2, length(r), 'double');
 for k=1:length(r)
     i = r(k);
     j = c(k);
     d = ceil(abs(dif(i, j)));
-    
     subdif = dif(i-d:i+d, j-d:j+d);
     if strcmp(metric, 'chessboard')
         fv = [subdif(1, :).'; subdif(2:end, end); flip(subdif(end, 1:end-1)).'; flip(subdif(2:end-1, 1))];
@@ -83,4 +74,12 @@ for k=1:length(r)
         [ra, ca] = find((subaux > (d - 1)) & (subaux <= d));
         fv = [diag(subdif(ra, ca + d)); diag(subdif(d + d + 2 - flip(ra(1:end-1)), flip(ca(1:end-1)) + d)); diag(subdif(d + d + 2 - ra(2:end), d + 2 - ca(2:end))); diag(subdif(flip(ra(2:end-1)), d + 2 - flip(ca(2:end-1))))];
     end
+    X(1:length(fv), k) = fv;
+    X(end-1, k) = d;
+    X(end, k) = length(fv);
+end
+%%
+for i=5:360:length(r)
+    sample = X(:, i);
+    figure; plot(sample(1:sample(end))); hold on; plot(ones(sample(end), 1)*sample(end-1));
 end
