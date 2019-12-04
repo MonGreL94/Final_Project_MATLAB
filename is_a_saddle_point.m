e7828a441b7e9702243d6a04554c55ea865366d3
MonGreL94 <a.scaldaferri2@studenti.unisa.it>
2019-12-04 12:55:29 +0100

    Update is_a_saddle_point.m

--- is_a_saddle_point.m
+++ is_a_saddle_point.m
@@ -2,14 +2,14 @@ function is_a_saddle = is_a_saddle_point(fv, v)
 is_a_saddle = false;
 [m1, im1] = min(fv);
 if m1 < v
-    fv = circshift(fv, -(im1 - 1));
-    im2 = (length(fv) / 2) + 1;
+    fv = circshift(fv, -im1);
+    im2 = length(fv) / 2;
     m2 = fv(im2);
     if m2 < v
         [M1, iM1] = max(fv);
         if M1 > v
-            fv = circshift(fv, -(iM1 - 1));
-            iM2 = (length(fv) / 2) + 1;
+            fv = circshift(fv, -iM1);
+            iM2 = length(fv) / 2;
             M2 = fv(iM2);
             if M2 > v
                 is_a_saddle = true;
