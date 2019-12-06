419e68eff05ebf90d09504f8f90338b773cc2938
MonGreL94 <a.scaldaferri2@studenti.unisa.it>
2019-12-06 10:31:41 +0100

    labelled-stack added and is_a_saddle_points_v2 changes

--- segment_by_segment_algorithm/is_a_saddle_point_v2.m
+++ segment_by_segment_algorithm/is_a_saddle_point_v2.m
@@ -1,11 +1,13 @@
 function is_a_saddle = is_a_saddle_point_v2(fv, v)
 is_a_saddle = false;
 mins = find(~fv);
-if length(mins) == 1
-    fv = circshift(fv, -(mins(1) - 1));
+% if length(mins) == 1
+for imin=1:length(mins)
+    fv = circshift(fv, -(mins(imin) - 1));
     values = fv(1:length(fv)/4:end);
-    if values(1) < v && values(2) > v && values(3) < v && values(4) > v
+    if (values(2) == max(fv(:)) || values(4) == max(fv(:))) && v && values(2) > v && values(3) < v && values(4) > v
         is_a_saddle = true;
     end
 end
+% end
 end
