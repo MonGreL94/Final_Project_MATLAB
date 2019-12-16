72f543dd92e0bc298c02f2aeb7e1f5376ca65af6
MonGreL94 <a.scaldaferri2@studenti.unisa.it>
2019-12-16 10:16:07 +0100

    small and big dataset created

--- segment_by_segment_algorithm/dataset_segmentation.m
+++ segment_by_segment_algorithm/dataset_segmentation.m
@@ -1,16 +1,18 @@
 clear; close all; clc;
 
-files = dir('..\dataset');
+files = dir('..\big_dataset\images_dataset');
 for i=3:length(files)
-    img = imread('..\dataset\' + string(files(i).name));
-    if i >= 30
+    img = imread('..\big_dataset\images_dataset\' + string(files(i).name));
+    if i >= 3
         cropped_img = imcrop(img);
     else
         cropped_img = img;
     end
-    [thresholded_img, segmented_img, color_segmented_img] = cc_segm(cropped_img);
-    imwrite(cropped_img, '..\cropped_dataset\' + string(files(i).name));
-    imwrite(thresholded_img, '..\thresholded_dataset\' + string(files(i).name));
-    imwrite(imnorm(segmented_img), '..\segmented_dataset\' + string(files(i).name));
-    imwrite(color_segmented_img, '..\color_segmented_dataset\' + string(files(i).name));
+    if  isequal(string(files(i).name), 'Coins.jpg')
+        [thresholded_img, segmented_img, color_segmented_img] = cc_segm(cropped_img);
+        imwrite(cropped_img, '..\big_dataset\cropped_dataset\' + string(files(i).name));
+        imwrite(thresholded_img, '..\big_dataset\thresholded_dataset\' + string(files(i).name));
+        imwrite(imnorm(segmented_img), '..\big_dataset\segmented_dataset\' + string(files(i).name));
+        imwrite(color_segmented_img, '..\big_dataset\color_segmented_dataset\' + string(files(i).name));
+    end
 end
